142e29ab9145443e227313a3b5571d841c9c77a5
Marie Erin <m.oshaughnessy@outlook.com>
2020-07-29 13:53:00 -0400

    added header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+// Header
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>✧Tarot Universe✧</h1>
+                <span>Call upon the universe for insight and take a moment to reflect</span>
+            </header>
+        )
+    }
+}
+
+export default Header;
