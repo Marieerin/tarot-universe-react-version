dbd66c1d4573b9a241e42ff7f5ed9a3523c06c3c
Marie Erin <m.oshaughnessy@outlook.com>
2020-07-29 14:50:35 -0400

    Updates

---
+++ precache-manifest.fa3b22bff19fc752ec9240e4ff6ef1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3266624dc26edfa205b58138bc4728f6",
+    "url": "/tarot-universe-react-version/index.html"
+  },
+  {
+    "revision": "65023e1bf7c8635ef805",
+    "url": "/tarot-universe-react-version/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7d9c0b0f64aaa1c9468",
+    "url": "/tarot-universe-react-version/static/js/2.4646e72e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarot-universe-react-version/static/js/2.4646e72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65023e1bf7c8635ef805",
+    "url": "/tarot-universe-react-version/static/js/main.536412e8.chunk.js"
+  },
+  {
+    "revision": "ca99f404585f52124fb9",
+    "url": "/tarot-universe-react-version/static/js/runtime-main.f73fcd31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tarot-universe-react-version/static/media/logo.5d5d9eef.svg"
+  }
+]);
