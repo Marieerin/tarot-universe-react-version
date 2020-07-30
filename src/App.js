6175f94df931cbac93aa51eaa42a7fad1906df2f
Marie Erin <m.oshaughnessy@outlook.com>
2020-07-30 09:42:42 -0400

    README updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Header from './Header';
@@ -7,6 +6,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
